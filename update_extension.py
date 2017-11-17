7d24c498fd1e167dd335d5a641d659fe9dfca95a
Nick Sifniotis <nick.sifniotis@anu.edu.au>
2017-11-17 14:41:03 +1100

    Updated extension sync script to handle creation of directories that aren't there.

--- update_extension.py
+++ update_extension.py
@@ -6,9 +6,23 @@
 # The changes are pulled directly from the development folders on this computer.
 
 import dirsync
+import os
 
-dirsync.sync("/home/nsifniotis/PycharmProjects/sphinxcontrib-jupyter/sphinxcontrib",
-             "/home/nsifniotis/PycharmProjects/jupinx-extension-test/extension",
+extension_development_location = "/home/nsifniotis/PycharmProjects/sphinxcontrib-jupyter/sphinxcontrib"
+local_extension_copy = "extension/"
+
+if not os.path.isdir(extension_development_location):
+    print("Critical Error: Unable to locate the development copy of Jupinx. Are you sure it is in "
+          + extension_development_location + "?")
+    exit(1)
+
+if not os.path.isdir(local_extension_copy):
+    os.mkdir(local_extension_copy)
+    print("Created directory for local copy of extension.")
+
+dirsync.sync(extension_development_location,
+             local_extension_copy,
              'sync')
 
+print("Extension updated! Run make jupyter to test it.")
 # That's all folks!
